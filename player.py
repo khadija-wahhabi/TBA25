3c5d92c5f583ce733aca1058891e83480408293d
khadija-wahhabi <khadija.wahhabi@edu.esiee.fr>
2026-01-06 14:11:30 +0100

    Update player.py

--- player.py
+++ player.py
@@ -26,8 +26,9 @@ class Player:
     
     # Define the move method.
     def move(self, direction):
+        direction = direction.upper() 
         # Get the next room from the exits dictionary of the current room.
-        next_room = self.current_room.exits[direction]
+        next_room = self.current_room.exits.get(direction)
 
         # If the next room is None, print an error message and return False.
         if next_room is None:
