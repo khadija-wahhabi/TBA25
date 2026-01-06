289004345d72bf72249cdee1644ca0fbe9f84b10
khadija-wahhabi <khadija.wahhabi@edu.esiee.fr>
2026-01-06 15:20:22 +0100

    Create item.py

---
+++ item.py
@@ -0,0 +1,22 @@
+# Description: Item class
+
+class Item:
+    """
+    Classe représentant un objet que le joueur peut trouver dans le jeu.
+
+    Attributs:
+        name (str): Le nom de l'objet.
+        description (str): La description détaillée de l'objet.
+        weight (float): Le poids de l'objet en kg.
+
+    Méthodes:
+        __str__(): Retourne une représentation textuelle de l'objet.
+    """
+
+    def __init__(self, name, description, weight):
+        self.name = name
+        self.description = description
+        self.weight = weight
+
+    def __str__(self):
+        return f"{self.name} : {self.description} ({self.weight} kg)"
