1d72a2986ec78fe5c265fed41c4f1f5b195cd1fd
khadija-wahhabi <khadija.wahhabi@edu.esiee.fr>
2026-01-06 12:45:53 +0100

    Update player.py

--- player.py
+++ player.py
@@ -1,4 +1,23 @@
 # Define the Player class.
+    """
+    This class represents the player in the game.
+    The player has a name and is always located in one room at a time.
+
+    Attributes:
+        name (str): The name of the player.
+        current_room (Room): The room where the player is currently located.
+
+    Methods:
+        __init__(self, name): Initializes the player with a name.
+        move(self, direction): Moves the player to another room according to a direction.
+
+    Examples:
+        >>> from player import Player
+        >>> player = Player("Indiana")
+        >>> player.name
+        'Indiana'
+    """
+
 class Player():
 
     # Define the constructor.
