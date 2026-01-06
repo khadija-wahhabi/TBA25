f273af26d8fda6092e93fbb777e128fd2154642a
khadija-wahhabi <khadija.wahhabi@edu.esiee.fr>
2026-01-06 15:24:56 +0100

    Update room.py

--- room.py
+++ room.py
@@ -49,3 +49,11 @@ class Room:
     # Return a long description of this room including exits.
     def get_long_description(self):
         return f"\nVous êtes {self.description}\n\n{self.get_exit_string()}\n"
+
+    def get_inventory(self):
+    if not self.inventory:
+        return "Il n'y a rien ici."
+    s = "La pièce contient :"
+    for item in self.inventory.values():
+        s += f"\n    - {item}"
+    return s
