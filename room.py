50a1823d28e42ea714841e7cc95d80c5eb06f445
khadija-wahhabi <khadija.wahhabi@edu.esiee.fr>
2026-01-06 16:01:07 +0100

    Update room.py

--- room.py
+++ room.py
@@ -52,8 +52,6 @@ class Room:
         return f"\nVous êtes {self.description}\n\n{self.get_exit_string()}\n"
 
     def get_inventory(self):
-        if not hasattr(self, "inventory"):
-            self.inventory = {}  
         if not self.inventory:
             return "Il n'y a rien ici."
         s = "La pièce contient :"
