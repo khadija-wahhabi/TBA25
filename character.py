02165765e63ba1b0b398f29f574b20400ea9d692
khadija-wahhabi <khadija.wahhabi@edu.esiee.fr>
2026-01-15 11:38:25 +0100

    Update character.py

--- character.py
+++ character.py
@@ -10,12 +10,23 @@ class Character:
         current_room (Room): la pièce où se trouve le personnage
         msgs (list[str]): liste de messages que le personnage peut dire
     """
-
+    
     def __init__(self, name, description, current_room, msgs):
         self.name = name
         self.description = description
         self.current_room = current_room
-        self.msgs = msgs
+        self.msgs = msgs[:] 
 
     def __str__(self):
         return f"{self.name} : {self.description}"
+
+    def get_msg(self):
+        if not self.msgs:
+            print(f"{self.name} ne dit rien.")
+            return None
+
+        msg = self.msgs.pop(0)
+        print(msg)
+        self.msgs.append(msg)
+        return msg
+
