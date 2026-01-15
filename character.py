68ff133378d016aceaa37af824c33c5cc654d547
khadija-wahhabi <khadija.wahhabi@edu.esiee.fr>
2026-01-15 10:59:25 +0100

    Create character.py

---
+++ character.py
@@ -0,0 +1,21 @@
+# Description: Character class
+
+class Character:
+    """
+    Classe représentant un personnage non joueur (PNJ).
+
+    Attributs:
+        name (str): le nom du personnage
+        description (str): la description du personnage
+        current_room (Room): la pièce où se trouve le personnage
+        msgs (list[str]): liste de messages que le personnage peut dire
+    """
+
+    def __init__(self, name, description, current_room, msgs):
+        self.name = name
+        self.description = description
+        self.current_room = current_room
+        self.msgs = msgs
+
+    def __str__(self):
+        return f"{self.name} : {self.description}"
